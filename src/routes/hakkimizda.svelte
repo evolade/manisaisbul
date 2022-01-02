8217d93d478bc668d4fb0bac98dbfd6dc700e27c
evolade <kutaycereci89@gmail.com>
2022-01-02 23:43:28 +0300

    update

--- src/routes/hakkimizda.svelte
+++ src/routes/hakkimizda.svelte
@@ -1,1 +1,10 @@
-<h1>hakkımızda...</h1>
+<h2>İŞBUL, 2016 yılından beri hem iş arayanlara hem de işverenlere profesyonel hizmetler sunmaktadır. Uzun ömürlü iş hayatımızda öğrendik ki, insan kaynakları sayılar ile alakalı bir sektör değil. Tam tersine, güzel ilişkiler kurularak yönetilmesi gereken bir süreç. Biz de bu yüzden dinlemeyi, rehberlik etmeyi ve doğru eşleştirmeler yapmayı bir öncelik olarak görüyoruz.
+Günümüzün dinamik ve rekabetçi ortamında kıt kaynaklardan etkin bir biçimde yararlanarak mal ve hizmet üretmek daha da önem kazanmaktadır. Üretimin en önemli unsuru ise insan emeğidir. Bunun bilince olan İŞBUL İnsan Kaynakları, işletmeler/kurumlar ile en iyi yeteneğe sahip kişileri buluşturmayı temel amaç edinmiş bir insan kaynakları firmasıdır. Günümüz bilgi çağından ürünler ve pazarlar daha az önem kazanırken, yetkin insan kaynağı daha da önem kazanmaktadır.
+İŞBUL İK bu anlamda işletmelere/kurumlar için rekabet avantajı sağlayacak bir vizyon ortağı olan insan kaynakları firmasıdır. Geniş bir portföye sahip olan İŞBUL İK, gerek iş arayanlar gerekse işverenler açısından en iyi hizmeti sunarak çözüm üreten bir firma olarak anılmaya devam edecektir.
+Başta Manisa olmak üzere bir çok bölge ve şehirde hizmet veren İŞBUL hizmet ağı en geniş İnsan Kaynakları firması olma gayesinde şubeleşme çalışmalarına hızla devam etmektedir.</h2>
+
+<style>
+    h2 {
+        margin: 100px;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,26 @@
+<script>
+	import { fade } from 'svelte/transition';
+	let visible = true;
 
+</script>
 <div class="imgbox">
-    <img src="corp2.jpg" alt="" class="containera">
+	<img src="man.jpeg" alt="" class="containera">
+	
+	<div class="bll">
+		<h1 class="bl">En Büyük</h1>
+		<h1 class="bl">Maliyet Zamandır...</h1>
+		
+		<h1 class="bls">Doğru Personel Yerleştirmeşi ile Firmanıza Zaman Kazandırmak İçin Varız</h1>
 
+	</div>
     
 </div>
+
+{#if visible}
+	<p transition:fade>
+		Fades in and out
+	</p>
+{/if}
 
 
 <style>
@@ -19,6 +36,69 @@
   width: 100%;
   
 }
+
+	.agu {
+		transition: opacity 2s;
+    -webkit-transition: opacity 2s; /* Safari */
+	}
+
+
+	.bll  {
+		
+		
+		position: absolute;
+	top: 44%;
+	
+	left: 16px;
+	
+
+}
+
+
+	@keyframes fadein {
+		from { opacity: 0; }
+		to   { opacity: 1; }
+	}
+	@-webkit-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+
+	.bl {
+	
+
+    -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein 2s; /* Firefox < 16 */
+        -ms-animation: fadein 2s; /* Internet Explorer */
+         -o-animation: fadein 2s; /* Opera < 12.1 */
+            animation: fadein 2s;
+	
+
+	font-family: sans-serif;
+	color: white;
+	font-size: 100%;
+	margin-left: 100px;
+	
+	-webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: black;
+	}	
+	.bls {
+	
+
+    -webkit-animation: fadein 50s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein 30s; /* Firefox < 16 */
+        -ms-animation: fadein 30s; /* Internet Explorer */
+         -o-animation: fadein 30s; /* Opera < 12.1 */
+            animation: fadein 5s;
+	
+
+	font-family: sans-serif;
+	margin-left: 100px;
+	color: white;
+	font-size: 100%;
+	-webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: black;
+	}	
 
 
 </style>
