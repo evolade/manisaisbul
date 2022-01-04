23cce793c20d88573f888bf7bdd976d271f1616b
evolade <kutaycereci89@gmail.com>
2022-01-04 16:39:37 +0300

    u

--- src/routes/iletisim.svelte
+++ src/routes/iletisim.svelte
@@ -7,10 +7,18 @@
 <a href="https://www.facebook.com/groups/1623869321162341">
     <img src="/fb.png" alt="" class="images">
 </a>
+<br>
+<br>
+<a href="https://www.google.com/maps/dir//Mimar+Sinan,+Ulupark,+Al%C3%A7%C4%B1tepe+Cd.+No:7,+45010+Manisa+Merkez%2FManisa/@38.6142012,27.4250169,17z/data=!4m16!1m6!3m5!1s0x14b99c6a2933b853:0x310884b75bd20d6e!2sUlupark!8m2!3d38.6135479!4d27.426423!4m8!1m0!1m5!1m1!1s0x14b99c6a2933b853:0x310884b75bd20d6e!2m2!1d27.426423!2d38.6135479!3e3x">
+    <img src="/maps.jpeg" alt="" class="maps">
+</a>
 
 
 <style>
     .images {
         height: 50px;
+    }
+    .maps {
+        height: 200px;
     }
 </style>
